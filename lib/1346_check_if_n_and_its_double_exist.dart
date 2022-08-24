5ff0b8b3f82da9b94c8c9b617a586de4e4c9e1f5
Dmitry Seloustev <seloustevdv@elocont.ru>
2022-08-24 21:49:33 +0700

    13. Roman to Integer

--- lib/1346_check_if_n_and_its_double_exist.dart
+++ lib/1346_check_if_n_and_its_double_exist.dart
@@ -5,8 +5,8 @@ class CheckIfNAndItsDoubleExist {
       map[arr[i]] = i;
     }
     for (var i = 0; i < arr.length; i++) {
-      final value = arr[i] * 2;
-      if (map.containsKey(value) && map[value] != i) {
+      final x = arr[i] << 1;
+      if (map.containsKey(x) && map[x] != i) {
         return true;
       }
     }
@@ -18,15 +18,16 @@ class CheckIfNAndItsDoubleExist {
       return false;
     }
     for (var i = 0; i < arr.length - 1; ++i) {
-      final x = arr[i] * 2;
-      final y = arr[i] / 2;
+      final value = arr[i];
+      final x = value <<  1;
+      final y = value >>> 1;
       if (x == 0) {
         for (var j = i + 1; j < arr.length; ++j) {
           if (arr[j] == 0) {
             return true;
           }
         }
-      } else if (y == y.truncate()) {
+      } else if (y == value) {
         for (var j = i + 1; j < arr.length; ++j) {
           if (arr[j] == x || arr[j] == y) {
             return true;
